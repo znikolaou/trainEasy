318118c29c7c72e818e09ed5ed758b46cef5516f
Zacharias <ZachariasMNic@gmail.com>
2026-03-14 08:23:29 +0200

    Added 2.8 billion parameter 16-layer example.

---
+++ examples/main16LayerAnn.py
@@ -0,0 +1,16 @@
+import libs
+from network import *
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.losses as losses
+import tensorflow.keras.metrics as metrics
+
+#Specify network architecture
+nLayers=2**4
+layerList=[]
+for i in range(nLayers):
+    layerList.append(2**(nLayers-i))
+
+#Build model
+fcModel=FcNetwork(layerList)
+
